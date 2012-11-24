760c066e3232c3ee46f7aa2de2eb29a077f26cab
Dan Higham <dhigham@rbcon.com>
2012-11-24 12:30:24 -0800

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  # map.namespace :admin, :member => { :remove => :get } do |admin|
-  #   admin.resources :image_gallery
-  # end
+  map.connect 'image_gallery/index', :controller => 'image_gallery', :action => 'index'   
+  map.connect 'image_gallery/upload', :controller => 'image_gallery', :action => 'upload'
+  map.connect 'image_gallery/delete_image', :controller => 'image_gallery', :action => 'delete_image'       
+  map.connect 'image_gallery/delete_collection', :controller => 'image_gallery', :action => 'delete_collection'       
+  map.connect 'image_gallery/reorder_images', :controller => 'image_gallery', :action => 'reorder_images'
+  map.connect 'image_gallery/reorder_collections', :controller => 'image_gallery', :action => 'reorder_collections'
 end
--- image_gallery_extension.rb
+++ image_gallery_extension.rb
@@ -5,16 +5,7 @@ class ImageGalleryExtension < Radiant::Extension
   version "1.0"
   description "A simple image gallery that makes use of drag and drop uploading"
   url "http://yourwebsite.com/image_gallery"
-  
-  define_routes do |map|
-    map.connect 'image_gallery/index', :controller => 'image_gallery', :action => 'index'   
-    map.connect 'image_gallery/upload', :controller => 'image_gallery', :action => 'upload'
-    map.connect 'image_gallery/delete_image', :controller => 'image_gallery', :action => 'delete_image'       
-    map.connect 'image_gallery/delete_collection', :controller => 'image_gallery', :action => 'delete_collection'       
-    map.connect 'image_gallery/reorder_images', :controller => 'image_gallery', :action => 'reorder_images'
-    map.connect 'image_gallery/reorder_collections', :controller => 'image_gallery', :action => 'reorder_collections'
-  end
-  
+    
   extension_config do |config|
        
       config.gem 'paperclip'
